b4640443f82ef9765f170aed9cca582ffab0e6b2
sunnnytoast <evefraczkiewicz@gmail.com>
2022-05-19 16:31:23 -0700

    added general tests for the spreadsheet

--- test.cpp
+++ test.cpp
@@ -1,7 +1,34 @@
 #include "spreadsheet.hpp"
-
+#include "select.hpp"
 #include "gtest/gtest.h"
 
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+TEST(Spreadsheet, AddToSheet) {
+	Spreadsheet sheet;
+	stringstream test;
+	
+	sheet.set_column_names({"Name", "Age", "ID"});
+	sheet.add_row({"Eden", "20", "173694"});
+
+	sheet.print_selection(test);
+	EXPECT_EQ(test.str(), "Eden 20 173694 \n");
+}
+
+TEST(Spreadsheet, EmptyRow) {
+        Spreadsheet sheet;
+        stringstream test;
+
+        sheet.set_column_names({"Name", "Age", "ID"});
+        sheet.add_row({"", "", ""});
+
+        sheet.print_selection(test);
+        EXPECT_EQ(test.str(), "   \n");
+
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
