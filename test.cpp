7a30954c484bd655feeb39a18a33bc7a0bde4cd5
Eden Fraczkiewicz <102642562+edenfraczkiewicz@users.noreply.github.com>
2022-05-19 16:35:28 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -7,6 +7,29 @@
 
 using namespace std;
 
+TEST(Spreadsheet, AddToSheet) {
+	Spreadsheet sheet;
+	stringstream test;
+	
+	sheet.set_column_names({"Name", "Age", "ID"});
+	sheet.add_row({"Eden", "20", "173694"});
+
+	sheet.print_selection(test);
+	EXPECT_EQ(test.str(), "Eden 20 173694 \n");
+}
+
+TEST(Spreadsheet, EmptyRow) {
+        Spreadsheet sheet;
+        stringstream test;
+
+        sheet.set_column_names({"Name", "Age", "ID"});
+        sheet.add_row({"", "", ""});
+
+        sheet.print_selection(test);
+        EXPECT_EQ(test.str(), "   \n");
+
+}
+
 TEST(Contains, True) {
 	Spreadsheet sheet;
 	stringstream test;
